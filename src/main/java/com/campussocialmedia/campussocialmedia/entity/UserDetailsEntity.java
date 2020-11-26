9663d74e7a0bdab4f17c9d973c5b3d1630fb77ce
Satvik Dandale <45892894+SatvikDandale@users.noreply.github.com>
2020-11-26 15:32:06 +0530

    Return profile Photo after login

--- src/main/java/com/campussocialmedia/campussocialmedia/entity/UserDetailsEntity.java
+++ src/main/java/com/campussocialmedia/campussocialmedia/entity/UserDetailsEntity.java
@@ -4,64 +4,81 @@ import java.util.HashMap;
 import java.util.List;
 
 public class UserDetailsEntity {
-	
+
 	private String userName;
 	private String email;
 	private String firstName;
 	private String lastName;
 	private String intro;
-	
-	
+	private String profilePhotoURL;
+
 	public UserDetailsEntity() {
-		super();	
+		super();
 	}
-	public UserDetailsEntity(String userName, String email, String firstName, String lastName,
-			String intro) {
+
+	public UserDetailsEntity(String userName, String email, String firstName, String lastName, String intro,
+			String profilePhotoURL) {
 		super();
 		this.userName = userName;
 		this.email = email;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.intro = intro;
+		this.profilePhotoURL = profilePhotoURL;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getIntro() {
 		return intro;
 	}
+
 	public void setIntro(String intro) {
 		this.intro = intro;
 	}
+
+	public String getProfilePhotoURL() {
+		return profilePhotoURL;
+	}
+
+	public void setProfilePhotoURL(String profilePhotoURL) {
+		this.profilePhotoURL = profilePhotoURL;
+	}
+
 	@Override
 	public String toString() {
-		return "UserDetailsEntity [userName=" + userName + ", email=" + email + ", firstName=" + firstName
-				+ ", lastName=" + lastName + ", intro=" + intro + "]";
+		return "UserDetailsEntity [email=" + email + ", firstName=" + firstName + ", intro=" + intro + ", lastName="
+				+ lastName + ", profilePhotoURL=" + profilePhotoURL + ", userName=" + userName + "]";
 	}
-	
-	
-	
-	
 
 }
