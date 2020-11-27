0105771941e2dfbaca3b42843203c1c90cebecca
Satvik Dandale <45892894+SatvikDandale@users.noreply.github.com>
2020-11-27 14:12:56 +0530

    Shift to dynamodb online

--- src/main/java/com/campussocialmedia/campussocialmedia/config/DynamoDBConfig.java
+++ src/main/java/com/campussocialmedia/campussocialmedia/config/DynamoDBConfig.java
@@ -7,8 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
+
+import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -16,18 +19,29 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 @Configuration
 public class DynamoDBConfig {
 
-	@Value("${amazon.access.key}")
-	private String awsAccessKey;
+	// @Value("${amazon.access.key}")
+	// private String awsAccessKey;
 
-	@Value("${amazon.access.secret-key}")
-	private String awsSecretKey;
+	// @Value("${amazon.access.secret-key}")
+	// private String awsSecretKey;
 
-	@Value("${amazon.end-point.url}")
-	private String awsDynamoDBEndPoint;
+	// @Value("${amazon.end-point.url}")
+	// private String awsDynamoDBEndPoint;
+
+	@Value("${amazonProperties.endpointUrl}")
+	private String endpointUrl;
+	@Value("${amazonProperties.bucketName}")
+	private String bucketName;
+	@Value("${amazonProperties.accessKey}")
+	private String accessKey;
+	@Value("${amazonProperties.secretKey}")
+	private String secretKey;
+	@Value("${amazonProperties.sessionToken}")
+	private String sessionToken;
 
 	@Bean
 	public AWSCredentials amazonAWSCredentials() {
-		return new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+		return new BasicSessionCredentials(this.accessKey, this.secretKey, this.sessionToken);
 	}
 
 	public AWSCredentialsProvider amazonAWSCredentialsProvider() {
@@ -36,8 +50,7 @@ public class DynamoDBConfig {
 
 	@Bean
 	public AmazonDynamoDB amazonDynamoDB() {
-		return AmazonDynamoDBClientBuilder.standard()
-				.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(awsDynamoDBEndPoint, ""))
+		return AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1)
 				.withCredentials(amazonAWSCredentialsProvider()).build();
 	}
 
