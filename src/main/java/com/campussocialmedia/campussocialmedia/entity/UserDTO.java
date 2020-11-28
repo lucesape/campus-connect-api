7e660a707cd7750a308742882d9a72921afb489b
Satvik Dandale <45892894+SatvikDandale@users.noreply.github.com>
2020-11-28 10:56:30 +0530

    Connected to config server on heroku

--- src/main/java/com/campussocialmedia/campussocialmedia/entity/UserDTO.java
+++ src/main/java/com/campussocialmedia/campussocialmedia/entity/UserDTO.java
@@ -3,19 +3,6 @@ package com.campussocialmedia.campussocialmedia.entity;
 import java.util.HashMap;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Data
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class UserDTO {
 	// private String userId;
 	private String userName;
